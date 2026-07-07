ee32ed569e5402e6e5f6e9aef58c7461e67611d1
Eliana Sobel <elianasobel610@gmail.com>
2026-07-06 23:23:47 -0400

    more gallery edits, make larger, add frame shake

--- components/contactform.tsx
+++ components/contactform.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 function ContactForm() {
     return (
-        <>
-            <div id="contact-form">
-                <h3>Eliana&#39;s Contact Information</h3>
-                <p>elianasobel610-at-gmail.com</p>
+        <div id="contact-form">
+            <h3>Get in touch</h3>
+            <div id="contact-links">
+                <a href="mailto:elianasobel610@gmail.com">elianasobel610@gmail.com</a>
                 <Link href="https://www.linkedin.com/in/eliana-sobel-4b1801289/">LinkedIn</Link>
             </div>
-        </>
+        </div>
     );
 }
 
